51bc5501beca5e83432cf1dd7faa0f674996a027
Muigai <Muigai.Mwaura@Gmail.com>
2018-08-07 19:36:10 +0300

    Updated to latest rahisi

--- dist/routing.d.ts
+++ dist/routing.d.ts
@@ -1,8 +1,8 @@
-import { R } from "rahisi";
+import { R, Renderable } from "rahisi";
 import { F0, F1 } from "rahisi-type-utils";
-export declare const Link: (props: R.AnchorHTMLAttributes<HTMLAnchorElement>) => any;
+export declare const Link: (props: R.AnchorHTMLAttributes<HTMLAnchorElement>, children: Renderable[]) => any;
 export interface Route<T> {
     path: string;
     action: F1<Map<string | number, string>, T>;
 }
-export declare function resolve<T>(routes: Array<Route<T>>, noMatch: F0<T>): Promise<T>;
+export declare function resolve<T>(routes: Array<Route<T>>, noMatch: F0<T>): T;
--- dist/routing.js
+++ dist/routing.js
@@ -1,18 +1,10 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const history_1 = require("history");
 const pathToRegexp = require("path-to-regexp");
 const rahisi_1 = require("rahisi");
 const history = history_1.createBrowserHistory();
-exports.Link = (props) => {
+exports.Link = (props, children) => {
     const attributes = rahisi_1.React.getAttributes(props);
     attributes.push(new rahisi_1.OnHandlerA("click", (event) => {
         event.preventDefault();
@@ -21,7 +13,7 @@ exports.Link = (props) => {
             search: event.currentTarget.search,
         });
     }));
-    return new rahisi_1.BaseElement("a", attributes);
+    return new rahisi_1.BaseElement("a", attributes, children);
 };
 function matchURI(path, uri) {
     const keys = [];
@@ -39,18 +31,16 @@ function matchURI(path, uri) {
     return params;
 }
 function resolve(routes, noMatch) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const uri = history.location.pathname;
-        for (const route of routes) {
-            const params = matchURI(route.path, uri);
-            if (!params) {
-                continue;
-            }
-            const result = yield route.action(params);
-            return result;
+    const uri = history.location.pathname;
+    for (const route of routes) {
+        const params = matchURI(route.path, uri);
+        if (!params) {
+            continue;
         }
-        return noMatch();
-    });
+        const result = route.action(params);
+        return result;
+    }
+    return noMatch();
 }
 exports.resolve = resolve;
 //# sourceMappingURL=routing.js.map
